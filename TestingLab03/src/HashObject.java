e748085de4db6f9190d79854806fc58bba036bf4
steph-ball <60074804+steph-ball@users.noreply.github.com>
2020-10-31 20:10:39 -0600

    initial changes/methods

--- TestingLab03/src/HashObject.java
+++ TestingLab03/src/HashObject.java
@@ -1,4 +1,55 @@
 
-public class HashObject {
+public class HashObject <T>{
+	
+	private T object;
+	private int duplicateCount;
+	private int probeCount;
+	
+	// Constructor
+	/**
+	 * Takes in object and instantiates duplicate count
+	 * and probe count.
+	 * @param object
+	 */
+	public HashObject(T object) {
+		this.object = object;
+		this.duplicateCount = 0;
+		this.probeCount = 0;
+	}
+	
+	// Methods
+	
+	/**
+	 * Increments the duplicate count
+	 */
+	public void incrementDuplicateCount() {
+		this.duplicateCount ++;
+	}
+	
+	/**
+	 * Returns the current duplicate count
+	 * @return
+	 */
+	public int getDuplicateCount() {
+		return duplicateCount;
+	}
+	
+	/**
+	 * increments the probe count
+	 */
+	public void incrementProbeCount() {
+		this.probeCount ++;
+	}
+	
+	/**
+	 * Returns the probe count
+	 * @return
+	 */
+	public int getProbeCount() {
+		return probeCount;
+	}
 
+	public T getKey() {
+		return object;
+	}
 }
--- TestingLab03/src/HashTest.java
+++ TestingLab03/src/HashTest.java
@@ -1,7 +1,8 @@
 
 public class HashTest {
 	public static void main(String[]args) {
-		// this is a change
+		
+		
 		
 	}
 }
